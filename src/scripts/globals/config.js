91ff1c10da3ea094e05f2bc5c52046dcea17a043
wisnuarizky <wisnuarizky01@gmail.com>
2023-05-09 22:32:11 +0700

    first commit

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+  KEY: '09c4df024515bfb13044937053582d6c',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: 'MovieCatalogue-V1',
+};
+
+export default CONFIG;
